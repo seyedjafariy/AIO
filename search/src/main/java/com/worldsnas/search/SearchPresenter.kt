9b131e29448df917769638a129f7cbbc5973261e
worldsnas <worldsnas@gmail.com>
2019-06-24 22:16:54 +0430

    added scope for saerch presenter

--- search/src/main/java/com/worldsnas/search/SearchPresenter.kt
+++ search/src/main/java/com/worldsnas/search/SearchPresenter.kt
@@ -1,9 +1,11 @@
 package com.worldsnas.search
 
 import com.worldsnas.base.BasePresenter
+import com.worldsnas.daggercore.scope.FeatureScope
 import com.worldsnas.mvi.MviProcessor
 import javax.inject.Inject
 
+@FeatureScope
 class SearchPresenter @Inject constructor(
     processor: MviProcessor<SearchIntent, SearchResult>
 ) : BasePresenter<SearchIntent, SearchState, SearchResult>(processor, SearchState.idle()) {
