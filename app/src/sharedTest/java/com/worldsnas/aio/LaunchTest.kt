2efb73ab1ad6d29da5c6e97ca5292f8ab24590a4
worldsnas <worldsnas@gmail.com>
2019-08-09 15:42:34 +0430

    launch test now successfully runs on both robolectric and Device

--- app/src/sharedTest/java/com/worldsnas/aio/LaunchTest.kt
+++ app/src/sharedTest/java/com/worldsnas/aio/LaunchTest.kt
@@ -1,4 +1,4 @@
-package java.com.worldsnas.aio
+package com.worldsnas.aio
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
@@ -6,13 +6,21 @@ import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.worldsnas.aio.MainActivity
-import com.worldsnas.aio.R
+import com.facebook.soloader.SoLoader
+import org.junit.BeforeClass
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
 class LaunchTest {
+
+    companion object {
+        @JvmStatic
+        @BeforeClass
+        fun setup() {
+            SoLoader.setInTestMode()
+        }
+    }
 
     @Test
     fun appLaunchesCorrectly() {
