ca97c7b612f632030a42aff27bea6a623e7b12ef
worldsnas <worldsnas@gmail.com>
2020-03-04 23:40:19 +0100

    added CoroutineView.kt converted homeView to CoroutineView.kt and ViewBindingController.kt
    builds fails with ButterKnife and ViewBinding complaining

---
+++ base/src/main/java/com/worldsnas/base/ViewBindingController.kt
@@ -0,0 +1,32 @@
+package com.worldsnas.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewbinding.ViewBinding
+
+abstract class ViewBindingController<T : ViewBinding> @JvmOverloads constructor(
+    bundle: Bundle? = null
+) : RefWatchingController(bundle) {
+
+    private var _binding: T? = null
+    val binding: T
+        get() = _binding!!
+
+    abstract fun bindView(inflater: LayoutInflater, container: ViewGroup) : T
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
+        _binding = bindView(inflater, container)
+        return binding.root
+    }
+
+    open fun onViewBound(binding : T){}
+    open fun unBindView(){}
+
+    override fun onDestroyView(view: View) {
+        _binding = null
+        super.onDestroyView(view)
+    }
+
+}
