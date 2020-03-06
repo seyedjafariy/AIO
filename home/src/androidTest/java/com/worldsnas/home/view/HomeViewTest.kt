bb25c402a4cd4ebcfeb931dd1a16088341846f8b
seyed.jafari <seyed.jafari@auto1.com>
2020-03-06 08:56:24 +0100

    coreComponent retrieval rethought

---
+++ home/src/androidTest/java/com/worldsnas/home/view/HomeViewTest.kt
@@ -0,0 +1,14 @@
+package com.worldsnas.home.view
+
+import com.worldsnas.home.HomePresenter
+import org.junit.Before
+
+import org.junit.Assert.*
+
+class HomeViewTest {
+
+    @Before
+    fun setUp() {
+        HomeView().presenter
+    }
+}
